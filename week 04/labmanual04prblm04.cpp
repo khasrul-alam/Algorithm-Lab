f8455b837fcc211519c36b9f9cb942282aff6304
khasrul-alam <49758973+khasrul-alam@users.noreply.github.com>
2020-06-27 22:44:37 +0600

    Create labmanual04prblm04.cpp

---
+++ week 04/labmanual04prblm04.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,j,n,k;
+    cout<<"Enter Array Size: ";
+    cin>>n;
+    int ara[n];
+    cout<<"Enter Data: ";
+    for(i=0; i<n; i++)
+        cin>>ara[i];
+
+    for(i=1; i<n; i++)
+    {
+        int x=ara[i];
+        j=i;
+        while(j>0 && ara[j-1]>ara[j])
+        {
+            swap(ara[j],ara[j-1]);
+            j--;
+        }
+
+    }
+
+
+    cout<<"Ascending Order: ";
+    for(i=0; i<n; i++)
+        cout<<ara[i]<<' ';
+    cout<<endl;
+    cout<<"Difference btn max-min= "<<ara[n-1]-ara[0]<<endl;
+    return 0;
+}
